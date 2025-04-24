867de5a6597dba887781c50e6c8dacd617b47c59
akzhy <akshay48884@gmail.com>
2025-04-24 22:27:55 +0530

    Create constants file

---
+++ packages/core/src/constants.ts
@@ -0,0 +1,9 @@
+export const CACHE_DIR_NAME = ".cache";
+
+export const STYLE_TAG_NAME = "Style";
+
+export const CLASSNAME_ATTRIBUTES = ["className", "classNames", "class"];
+
+export const CLASSNAME_UTIL_FUNCTIONS = ["c", "cn"];
+
+export const CLIENT_PACKAGE_NAMES = ["jsx-styled-react"];
