7f7c9537f2137ce43a3963c3b268073995c5b690
akzhy <akshay48884@gmail.com>
2025-04-24 22:31:12 +0530

    Update plugin to include cssPreprocessor support

--- packages/vite-plugin/src/index.ts
+++ packages/vite-plugin/src/index.ts
@@ -1,12 +1,20 @@
 import { transform } from "jsx-styled-core";
 import type { Plugin } from "vite";
 
-export default function jsxStyledVitePlugin(): Plugin {
+export default function jsxStyledVitePlugin({
+  cssPreprocessor,
+}: {
+  cssPreprocessor?: (css: string, id: string) => string;
+}): Plugin {
   return {
     name: "vite-plugin-jsx-styled",
     enforce: "pre",
     transform(code, id) {
-      return transform(code, id);
+      return transform({
+        code,
+        filePath: id,
+        cssPreprocessor,
+      });
     },
   };
 }
