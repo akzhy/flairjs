98847886e964f88123bc71508db92e1d0da608db
akzhy <akshay48884@gmail.com>
2025-07-16 12:37:34 +0530

    Add time logging

--- packages/flair-core/src/main.rs
+++ packages/flair-core/src/main.rs
@@ -1,11 +1,14 @@
 mod transform;
 mod parse_css;
 mod update_attribute;
-use std::fs;
+use std::{fs, time::Instant};
 use transform::{transform, TransformOptions};
 
 fn main() {
   let contents = fs::read_to_string("src/App.tsx").expect("Something went wrong reading the file");
+  
+  let start = Instant::now();
+  
   let options = TransformOptions {
     code: contents,
     file_path: "App.tsx".to_string(),
@@ -14,8 +17,11 @@ fn main() {
     output_path: Some("./out".to_string()), // Specify the output path
   };
 
+  let duration = start.elapsed();
+
   if let Some(result) = transform(options) {
     println!("Transformed code:\n{}", result);
+    println!("Transformation took: {:?}", duration);
   } else {
     println!("Transformation failed or skipped.");
   }
