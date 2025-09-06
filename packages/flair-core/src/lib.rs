09dba803743614c8d0a7e2c8b61fd1c22b7d7a10
Akshay <akshay48884@gmail.com>
2025-09-06 22:11:43 +0530

    Include time logging for transformation

--- packages/flair-core/src/lib.rs
+++ packages/flair-core/src/lib.rs
@@ -1,5 +1,7 @@
 #![deny(clippy::all)]
 
+use std::time::Instant;
+
 use napi::{Env, JsFunction};
 
 use crate::transform::{TransformOptions, TransformOutput};
@@ -7,22 +9,28 @@ use crate::transform::{TransformOptions, TransformOutput};
 #[macro_use]
 extern crate napi_derive;
 
-pub mod transform;
+pub mod flair_property;
 pub mod parse_css;
+pub mod style_tag;
+pub mod transform;
 pub mod update_attribute;
 
-#[napi]
-pub fn sum(a: i32, b: i32) -> i32 {
-  a + b
-}
 
 #[napi]
-pub fn transform_code(env: Env, options: TransformOptions, css_preprocessor: Option<JsFunction>) -> Option<TransformOutput> {
+pub fn transform_code(
+  env: Env,
+  options: TransformOptions,
+  css_preprocessor: Option<JsFunction>,
+) -> Option<TransformOutput> {
+  let time = Instant::now();
+  println!("Starting transformation...");
   let options = transform::TransformOptions {
     code: options.code,
     file_path: options.file_path,
     css_out_dir: options.css_out_dir,
   };
-  
-  transform::transform(options, css_preprocessor, Some(env))
+  let result = transform::transform(options, css_preprocessor, Some(env));
+  let duration = time.elapsed();
+  println!("Transformation completed in {:?}", duration);
+  result
 }
