74d3228a788203c2fc65eefbde7d01908496f70c
Akshay <akshay48884@gmail.com>
2025-09-08 23:02:37 +0530

    Add test case for flair object

--- packages/core/__test__/index.spec.ts
+++ packages/core/__test__/index.spec.ts
@@ -10,6 +10,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 const styleTagContent = readFileSync(path.resolve(__dirname, './utils/style-tag.tsx'), 'utf-8')
 const flairPropertyContent = readFileSync(path.resolve(__dirname, './utils/flair-string.tsx'), 'utf-8')
+const flairPropertyObjectContent = readFileSync(path.resolve(__dirname, './utils/flair-obj.tsx'), 'utf-8')
 
 test('style Tag is working', () => {
   const result = transformCode({
@@ -26,6 +27,18 @@ test('style Tag is working', () => {
 test('flair property string is working', () => {
   const result = transformCode({
     code: flairPropertyContent,
+    filePath: 'index.tsx',
+    cssOutDir: path.resolve(__dirname, './.css'),
+  })
+  if (!result) {
+    throw new Error('transformCode returned null or undefined')
+  }
+  expect(result.code).toMatchSnapshot()
+});
+
+test('flair property object is working', () => {
+  const result = transformCode({
+    code: flairPropertyObjectContent,
     filePath: 'index.tsx',
     cssOutDir: path.resolve(__dirname, './.css'),
   })
