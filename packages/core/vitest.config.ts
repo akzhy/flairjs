c49d3f34d2f21cef3fd0a3fb16a416879fcd7877
Akshay <akshay48884@gmail.com>
2025-09-08 21:45:29 +0530

    Replace ava with vitest

---
+++ packages/core/vitest.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    include: ['__test__/**/*.spec.ts'],
+    env: {
+      OXC_TSCONFIG_PATH: './__test__/tsconfig.json'
+    }
+  },
+  esbuild: {
+    loader: 'ts',
+    target: 'node18'
+  }
+})
