9a68d74ab3e6b508f4f8d3a4080cf71e50e47002
Akshay <akshay48884@gmail.com>
2025-09-08 21:45:40 +0530

    Add basic test cases

--- packages/core/__test__/index.spec.ts
+++ packages/core/__test__/index.spec.ts
@@ -1,8 +1,36 @@
-import test from 'ava'
+import { test, expect } from 'vitest'
 
-import { plus100 } from '../index'
+import { transformCode } from '../index'
+import { readFileSync } from 'node:fs'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+import path from 'node:path'
 
-test('sync function from native code', (t) => {
-  const fixture = 42
-  t.is(plus100(fixture), fixture + 100)
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const styleTagContent = readFileSync(path.resolve(__dirname, './utils/style-tag.tsx'), 'utf-8')
+const flairPropertyContent = readFileSync(path.resolve(__dirname, './utils/flair-string.tsx'), 'utf-8')
+
+test('style Tag is working', () => {
+  const result = transformCode({
+    code: styleTagContent,
+    filePath: 'index.tsx',
+    cssOutDir: path.resolve(__dirname, './.css'),
+  })
+  if (!result) {
+    throw new Error('transformCode returned null or undefined')
+  }
+  expect(result.code).toMatchSnapshot()
 })
+
+test('flair property string is working', () => {
+  const result = transformCode({
+    code: flairPropertyContent,
+    filePath: 'index.tsx',
+    cssOutDir: path.resolve(__dirname, './.css'),
+  })
+  if (!result) {
+    throw new Error('transformCode returned null or undefined')
+  }
+  expect(result.code).toMatchSnapshot()
+});
