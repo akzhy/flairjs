4282ad39f14dcf29b5e499909af0739b4a5e84b3
Akshay <akshay48884@gmail.com>
2025-09-10 21:42:32 +0530

    Add test case for custom classname attributes

--- packages/core/__test__/index.spec.ts
+++ packages/core/__test__/index.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from 'vitest'
+import { test, expect, describe } from 'vitest'
 
 import { transformCode } from '../index'
 import { readdirSync, readFileSync } from 'node:fs'
@@ -8,53 +8,47 @@ import path from 'node:path'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
-const styleTagContent = readFileSync(path.resolve(__dirname, './utils/style-tag.tsx'), 'utf-8')
-const flairPropertyContent = readFileSync(path.resolve(__dirname, './utils/flair-string.tsx'), 'utf-8')
-const flairPropertyObjectContent = readFileSync(path.resolve(__dirname, './utils/flair-obj.tsx'), 'utf-8')
+const styleTagContent = readFileSync(path.resolve(__dirname, './snippets/style-tag.tsx'), 'utf-8')
+const flairPropertyContent = readFileSync(path.resolve(__dirname, './snippets/flair-string.tsx'), 'utf-8')
+const flairPropertyObjectContent = readFileSync(path.resolve(__dirname, './snippets/flair-obj.tsx'), 'utf-8')
 
-test('style Tag is working', () => {
-  const result = transformCode({
-    code: styleTagContent,
-    filePath: 'index.tsx',
-    cssOutDir: path.resolve(__dirname, './.css'),
+describe('Core tests', () => {
+  test('style tag is working', () => {
+    const result = transformCode(styleTagContent, 'index.tsx', {
+      cssOutDir: path.resolve(__dirname, './.css'),
+    })
+    if (!result) {
+      throw new Error('transformCode returned null or undefined')
+    }
+    expect(result.code).toMatchSnapshot()
   })
-  if (!result) {
-    throw new Error('transformCode returned null or undefined')
-  }
-  expect(result.code).toMatchSnapshot()
+
+  test('flair property string is working', () => {
+    const result = transformCode(flairPropertyContent, 'index.tsx', {
+      cssOutDir: path.resolve(__dirname, './.css'),
+    })
+    if (!result) {
+      throw new Error('transformCode returned null or undefined')
+    }
+    expect(result.code).toMatchSnapshot()
+  })
+
+  test('flair property object is working', () => {
+    const result = transformCode(flairPropertyObjectContent, 'index.tsx', {
+      cssOutDir: path.resolve(__dirname, './.css'),
+    })
+    const cssFiles = readdirSync(path.resolve(__dirname, './.css')).filter((f) => f.endsWith('.css'))
+    const cssContent = readFileSync(path.resolve(__dirname, './.css', cssFiles[0]), 'utf-8')
+
+    transformCode(flairPropertyContent, 'index.tsx', {
+      cssOutDir: path.resolve(__dirname, './.css'),
+    })
+    const styleTagCssContent = readFileSync(path.resolve(__dirname, './.css', cssFiles[0]), 'utf-8')
+    expect(cssContent).toBe(styleTagCssContent)
+
+    if (!result) {
+      throw new Error('transformCode returned null or undefined')
+    }
+    expect(result.code).toMatchSnapshot()
+  })
 })
-
-test('flair property string is working', () => {
-  const result = transformCode({
-    code: flairPropertyContent,
-    filePath: 'index.tsx',
-    cssOutDir: path.resolve(__dirname, './.css'),
-  })
-  if (!result) {
-    throw new Error('transformCode returned null or undefined')
-  }
-  expect(result.code).toMatchSnapshot()
-});
-
-test('flair property object is working', () => {
-  const result = transformCode({
-    code: flairPropertyObjectContent,
-    filePath: 'index.tsx',
-    cssOutDir: path.resolve(__dirname, './.css'),
-  })
-  const cssFiles = readdirSync(path.resolve(__dirname, './.css')).filter(f => f.endsWith('.css'));
-  const cssContent = readFileSync(path.resolve(__dirname, './.css', cssFiles[0]), 'utf-8');
-
-  transformCode({
-    code: styleTagContent,
-    filePath: 'index.tsx',
-    cssOutDir: path.resolve(__dirname, './.css'),
-  })
-  const styleTagCssContent = readFileSync(path.resolve(__dirname, './.css', cssFiles[0]), 'utf-8');
-  expect(cssContent).toBe(styleTagCssContent)
-  
-  if (!result) {
-    throw new Error('transformCode returned null or undefined')
-  }
-  expect(result.code).toMatchSnapshot()
-});
---
+++ packages/core/__test__/snippets/class-name-list.tsx
@@ -0,0 +1,40 @@
+import clsx from 'clsx'
+import { flair } from '@flairjs/react'
+
+export const TestCaseComponent = () => {
+  return (
+    <>
+      <div>
+        <Box containerClassName="item"></Box>
+        <RegexBox classList={['regex-box']} />
+      </div>
+    </>
+  )
+}
+
+const Box = ({ children, containerClassName }: { children?: React.ReactNode; containerClassName?: string }) => {
+  return <div className={clsx('box', containerClassName)}>{children}</div>
+}
+
+const RegexBox = ({ children, classList }: { children?: React.ReactNode; classList?: string[] }) => {
+  return <div className={clsx('regex-box', classList)}>{children}</div>
+}
+
+Box.flair = flair({
+  '.box': {
+    padding: '10px',
+    border: '1px solid black',
+  },
+})
+
+TestCaseComponent.flair = flair({
+  '.item': {
+    width: '200px',
+    height: '200px',
+  },
+  '.regex-box': {
+    width: '100px',
+    height: '100px',
+    backgroundColor: 'lightblue',
+  },
+})
