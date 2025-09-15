09e9858700f8d55e716b5fb7ab2995aee1a78e84
Akshay <akshay48884@gmail.com>
2025-09-15 22:08:21 +0530

    Add theme config function

--- packages/core/api.ts
+++ packages/core/api.ts
@@ -1,3 +1,73 @@
-import { transformCode } from ".";
+import { transformCode } from '.'
 
-export { transformCode };
+export { transformCode }
+
+interface ThemeObjItem {
+  [key: string | number]: string | ThemeObjItem
+}
+
+export interface FlairThemeObject {
+  colors?: Record<string | number, string | ThemeObjItem>
+  space?: Record<string | number, string | ThemeObjItem>
+  fontSizes?: Record<string | number, string | ThemeObjItem>
+  fonts?: Record<string | number, string | ThemeObjItem>
+  fontWeights?: Record<string | number, string | ThemeObjItem>
+  lineHeights?: Record<string | number, string | ThemeObjItem>
+  letterSpacings?: Record<string | number, string | ThemeObjItem>
+  sizes?: Record<string | number, string | ThemeObjItem>
+  borderWidths?: Record<string | number, string | ThemeObjItem>
+  borderStyles?: Record<string | number, string | ThemeObjItem>
+  radii?: Record<string | number, string | ThemeObjItem>
+  shadows?: Record<string | number, string | ThemeObjItem>
+  zIndices?: Record<string | number, string | ThemeObjItem>
+  transitions?: Record<string | number, string | ThemeObjItem>
+}
+
+export type FlairThemeConfig = {
+  tokens: FlairThemeObject
+  breakpoints?: Record<string, string | number>
+  selector: string | ((content: string) => string)
+}
+
+export function defineConfig(config: FlairThemeConfig) {
+  return config
+}
+
+export const buildThemeTokens = (theme: FlairThemeConfig) => {
+  let css = ''
+  const { tokens, selector } = theme
+
+  if (typeof selector === 'string') {
+    css += `${selector} {\n`
+  }
+
+  css += tokensToCSSVars(tokens)
+
+  if (typeof selector === 'string') {
+    css += `}\n`
+  } else {
+    css = selector(css)
+  }
+
+  return css
+}
+
+type Tokens = {
+  [key: string]: string | number | Tokens
+}
+
+function tokensToCSSVars(tokens: Tokens | FlairThemeObject, prefix: string[] = []): string {
+  let css = ''
+
+  for (const [key, value] of Object.entries(tokens)) {
+    const newPrefix = [...prefix, key]
+
+    if (typeof value === 'string' || typeof value === 'number') {
+      css += `--${newPrefix.join('-')}: ${value};\n`
+    } else if (typeof value === 'object' && value !== null) {
+      css += tokensToCSSVars(value, newPrefix)
+    }
+  }
+
+  return css
+}
