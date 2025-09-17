4362eaf8656a522f68b109a01a0f36b26a5be628
Akshay <akshay48884@gmail.com>
2025-09-17 23:16:31 +0530

    Add rolldown config

---
+++ packages/vite-plugin/rolldown.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig, RolldownOptions } from 'rolldown';
+import typescript from '@rollup/plugin-typescript';
+
+const createOptions = (format: 'esm' | 'cjs'): RolldownOptions => {
+  return {
+    input: 'src/index.ts',
+    platform: 'node',
+    output: {
+      dir: `dist/${format}`,
+      format: format,
+      esModule: true,
+    },
+    external: (id) => {
+      return id.endsWith('.node') || id.includes('node_modules') || id == "@flairjs/core";
+    },
+  };
+};
+
+export default defineConfig([
+  createOptions('esm'),
+  createOptions('cjs'),
+  {
+    input: 'src/index.ts',
+    output: {
+      dir: 'dist/types',
+    },
+    plugins: [
+      typescript({ tsconfig: './tsconfig.json' }),
+    ],
+  },
+]);
