b0e4173f780c1ea5a467a55def78d65da2c29c3a
Akshay <akshay48884@gmail.com>
2026-02-10 22:21:55 +0530

    Improve CSS error logging with detailed messages and utility functions

--- packages/core/src/utils.rs
+++ packages/core/src/utils.rs
@@ -1,5 +1,8 @@
+use lightningcss::error::{Error, ErrorLocation, ParserError, PrinterErrorKind};
 use napi_derive::napi;
 use oxc_data_structures::rope::Rope;
+
+use crate::transform::CSSData;
 
 pub struct ExtendedRope<'a> {
   rope: Rope,
@@ -28,4 +31,97 @@ pub struct UnusedCss {
   pub class_name: String,
   pub line: u32,
   pub column: u32,
+}
+
+pub fn map_lightning_css_parser_error_to_string(error: Error<ParserError>) -> String {
+  match error.kind {
+    ParserError::AtRuleBodyInvalid => "At-rule body is invalid".to_string(),
+    ParserError::AtRulePreludeInvalid => "At-rule prelude is invalid".to_string(),
+    ParserError::AtRuleInvalid(rule_name) => {
+      format!("Unknown or unsupported at-rule: {}", rule_name)
+    }
+    ParserError::EndOfInput => "Unexpected end of input".to_string(),
+    ParserError::InvalidDeclaration => "Invalid declaration".to_string(),
+    ParserError::InvalidMediaQuery => "Invalid media query".to_string(),
+    ParserError::InvalidNesting => "Invalid CSS nesting".to_string(),
+    ParserError::DeprecatedNestRule => "The @nest rule is deprecated".to_string(),
+    ParserError::DeprecatedCssModulesValueRule => {
+      "The @value rule (CSS modules) is deprecated".to_string()
+    }
+    ParserError::InvalidPageSelector => "Invalid selector in @page rule".to_string(),
+    ParserError::InvalidValue => "Invalid value encountered".to_string(),
+    ParserError::QualifiedRuleInvalid => "Invalid qualified rule".to_string(),
+    ParserError::SelectorError(selector_err) => format!("Selector error: {:?}", selector_err),
+    ParserError::UnexpectedImportRule => {
+      "@import rule must come before all rules except @charset or @layer".to_string()
+    }
+    ParserError::UnexpectedNamespaceRule => {
+      "@namespace rule must come before all rules except @charset, @import, or @layer".to_string()
+    }
+    ParserError::UnexpectedToken(token) => format!("Unexpected token: {:?}", token),
+    ParserError::MaximumNestingDepth => "Maximum nesting depth reached".to_string(),
+  }
+}
+
+pub fn map_lightning_css_printer_error_to_string(error: Error<PrinterErrorKind>) -> String {
+  match error.kind {
+    PrinterErrorKind::AmbiguousUrlInCustomProperty { url } => {
+      format!("Ambiguous relative url() in custom property: {}", url)
+    }
+    PrinterErrorKind::FmtError => "Formatting error occurred".to_string(),
+    PrinterErrorKind::InvalidComposesNesting => {
+      "CSS modules 'composes' property cannot be used within nested rules".to_string()
+    }
+    PrinterErrorKind::InvalidComposesSelector => {
+      "CSS modules 'composes' property can only be used with a simple class selector".to_string()
+    }
+    PrinterErrorKind::InvalidCssModulesPatternInGrid => {
+      "CSS modules pattern must end with [local] for use in CSS grid".to_string()
+    }
+  }
+}
+
+fn format_file_info(loc: &Option<ErrorLocation>, filename: &str, css_data: &CSSData) -> String {
+  let line_number = if let Some(loc) = &loc {
+    css_data.line_number + loc.line
+  } else {
+    css_data.line_number
+  };
+  let column_number = if let Some(loc) = &loc {
+    if loc.line == 0 {
+      css_data.column_number + loc.column
+    } else {
+      loc.column
+    }
+  } else {
+    css_data.column_number
+  };
+
+  format!("{}:{}:{}", filename, line_number, column_number)
+}
+
+pub trait MapLightningCssError {
+  fn to_flair_error_string(self, filename: String, css_data: &CSSData) -> String;
+}
+
+impl<'a> MapLightningCssError for Error<ParserError<'a>> {
+  fn to_flair_error_string(self, filename: String, css_data: &CSSData) -> String {
+    let file_info = format_file_info(&self.loc, &filename, &css_data);
+    format!(
+      "{} {}",
+      file_info,
+      map_lightning_css_parser_error_to_string(self)
+    )
+  }
+}
+
+impl MapLightningCssError for Error<PrinterErrorKind> {
+  fn to_flair_error_string(self, filename: String, css_data: &CSSData) -> String {
+    let file_info = format_file_info(&self.loc, &filename, &css_data);
+    format!(
+      "{} {}",
+      file_info,
+      map_lightning_css_printer_error_to_string(self)
+    )
+  }
 }
